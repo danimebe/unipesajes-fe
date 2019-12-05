724f2b3735d2e5080beff1945e9a260906e542a3
danimebe <danielmb0515@gmail.com>
2019-12-04 19:46:44 -0500

    First commit - Boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+           template: './index.html',
+           filename: 'index.html' 
+        })
+    ]
+}
